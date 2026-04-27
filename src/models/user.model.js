20725a4653b0df55c60612455e8315a64a4d96aa
hemant <hemant.code09@gmail.com>
2026-04-28 03:22:49 +0530

    all setup configured

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,10 +51,11 @@ const userSchema = new Schema(
     }
 )
 
+//this pre hook is used to perform some action just before saving the data
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
